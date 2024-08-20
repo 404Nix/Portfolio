22eb699e6a67bbfa4dd019b0c0d9894426cc1c41
Nikhil kanojia <nikhilkanojia693@gmail.com>
2024-08-21 04:17:29 +0530

    fixed page3 height issue

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ var crsr = document.querySelector("#cursor");
 var crsrBlur = document.querySelector("#cursor-blur");
 
 document.addEventListener("mousemove", function(dets){
-    crsr.style.left = dets.x+"px";
-    crsr.style.top = dets.y+"px";
+    // crsr.style.left = dets.x+"px";
+    // crsr.style.top = dets.y+"px";
+    crsr.animate({
+        left : `${dets.x-5}px`,
+        top : `${dets.y-5}px`
+    }, {duration: 200, fill : "forwards"});
+
+
     crsrBlur.style.left = dets.x - 250 +"px";
     crsrBlur.style.top = dets.y - 250 +"px";
+
 })
 
 
